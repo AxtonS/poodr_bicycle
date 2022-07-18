612e33bd8017f0ffc9e737e01fbdfddee1eec270
Axton Smith <axtonsmith@gmail.com>
2022-07-17 21:51:45 -0400

    Create gear class

---
+++ lib/bicycle.rb
@@ -0,0 +1,19 @@
+# frozen_string_literal: true
+
+# calculates ratio for given gear
+class Gear
+  attr_reader :chainring, :cog
+
+  def initialize(chainring, cog)
+    @chainring = chainring
+    @cog = cog
+  end
+
+  def ratio
+    chainring / cog.to_f
+  end
+end
+
+puts Gear.new(52, 11).ratio
+
+puts Gear.new(30, 27).ratio
