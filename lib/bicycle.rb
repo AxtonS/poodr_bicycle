71ee12d394a2124d4206e1d7bda095f49b005e45
Axton Smith <axtonsmith@gmail.com>
2022-07-17 21:59:52 -0400

    Add gear_inches method to Gear class

--- lib/bicycle.rb
+++ lib/bicycle.rb
@@ -2,18 +2,24 @@
 
 # calculates ratio for given gear
 class Gear
-  attr_reader :chainring, :cog
+  attr_reader :chainring, :cog, :rim, :tire
 
-  def initialize(chainring, cog)
+  def initialize(chainring, cog, rim, tire)
     @chainring = chainring
     @cog = cog
+    @rim = rim
+    @tire = tire
   end
 
   def ratio
     chainring / cog.to_f
   end
+
+  def gear_inches
+    ratio * (rim + (tire * 2))
+  end
 end
 
-puts Gear.new(52, 11).ratio
+puts Gear.new(52, 11, 26, 1.5).gear_inches
 
-puts Gear.new(30, 27).ratio
+puts Gear.new(52, 11, 24, 1.25).gear_inches
