9efbad88ff2526bc85f4e123403317bba230cb1e
Axton Smith <axtonsmith@gmail.com>
2022-07-18 21:45:32 -0400

    Finish chapter 6, add inheritance

--- lib/bicycles.rb
+++ lib/bicycles.rb
@@ -2,37 +2,114 @@
 
 # creates bicycle objects
 class Bicycle
-  attr_reader :style, :size, :tape_color, :front_shock, :rear_shock
+  attr_reader :size, :chain, :tire_size
 
   def initialize(**opts)
-    @style = opts[:style]
     @size = opts[:size]
+    @chain = opts[:chain] || default_chain
+    @tire_size = opts[:tire_size] || default_tire_size
+    post_initialize(opts)
+  end
+
+  def spares
+    {
+      tire_size: tire_size,
+      chain: chain
+    }.merge(local_spares)
+  end
+
+  def default_tire_size
+    raise NotImplementedError, "#{self.class} should have implemented..."
+  end
+
+  def post_initialize(opts); end
+
+  def local_spares
+    {}
+  end
+
+  def default_chain
+    '11-speed'
+  end
+end
+
+# creates road bike objects
+class RoadBike < Bicycle
+  attr_reader :tape_color
+
+  def post_initialize(opts)
     @tape_color = opts[:tape_color]
+  end
+
+  def local_spares
+    { tape_color: tape_color }
+  end
+
+  def default_tire_size
+    '23'
+  end
+end
+
+# creates mountain bike objects
+class MountainBike < Bicycle
+  attr_reader :front_shock, :rear_shock
+
+  def post_initialize(opts)
     @front_shock = opts[:front_shock]
     @rear_shock = opts[:rear_shock]
   end
 
-  def spares
-    if style == :road
-      {
-        chain: '11-speed',
-        tire_size: '23',
-        tape_color: tape_color
-      }
-    else
-      {
-        chain: '11-speed',
-        tire_size: '2.1',
-        front_shock: front_shock
-      }
-    end
+  def local_spares
+    { front_shock: front_shock }
+  end
+
+  def default_tire_size
+    '2.1'
   end
 end
 
-bike = Bicycle.new(
-  style: :road,
+# creates recumbent bike objects
+class RecumbentBike < Bicycle
+  attr_reader :flag
+
+  def post_initialize(opts)
+    @flag = opts[:flag]
+  end
+
+  def local_spares
+    { flag: flag }
+  end
+
+  def default_chain
+    '10-speed'
+  end
+
+  def default_tire_size
+    '28'
+  end
+end
+
+road_bike = RoadBike.new(
   size: 'M',
   tape_color: 'red'
 )
+puts road_bike.tire_size
+puts road_bike.chain
+puts road_bike.spares
 
-puts bike.spares
+mountain_bike = MountainBike.new(
+  size: 'S',
+  front_shock: 'Manitou',
+  rear_shock: 'Fox'
+)
+
+puts mountain_bike.tire_size
+puts mountain_bike.chain
+puts mountain_bike.spares
+
+bent = RecumbentBike.new(
+  size: 'M',
+  flag: 'tall and orange'
+)
+
+puts bent.spares
