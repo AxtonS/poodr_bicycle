7b3e71e316c5861261b59c11920957d90e406be6
Axton Smith <axtonsmith@gmail.com>
2022-07-21 21:05:21 -0400

    Finish chapter 9 examples

---
+++ chapter9_testing/wheel_and_gear.rb
@@ -0,0 +1,35 @@
+# frozen_string_literal: true
+
+# class wheel
+class Wheel
+  attr_reader :rim, :tire
+
+  def initialize(rim, tire)
+    @rim = rim
+    @tire = tire
+  end
+
+  def diameter
+    rim + (tire * 2)
+  end
+end
+
+# class gear
+class Gear
+  attr_reader :chainring, :cog, :rim, :tire
+
+  def initialize
+    @chainring = chainring
+    @cog = cog
+    @rim = rim
+    @tire = tire
+  end
+
+  def gear_inches
+    ratio * Wheel.new(rim, tire).diameter
+  end
+
+  def ratio
+    chainring / cog.to_f
+  end
+end
